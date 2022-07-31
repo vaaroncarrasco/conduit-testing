d43b607bad83b57e44e1962a82f3f855380cb3af
Aaron Carrasco <vaaroncarrasco@gmail.com>
2022-07-31 17:37:38 -0500

    delete test added

---
+++ cypress/e2e/articles/article_delete.cy.js
@@ -0,0 +1,12 @@
+describe('delete articles', () => {
+  beforeEach(() => {
+    cy.login()
+  });
+
+  it('delete 1st article through UI', () => {
+    cy.visit(`/@${localStorage.getItem('username')}`);
+    cy.get(':nth-child(1) > .article-preview > .preview-link > p.ng-binding').click()
+    cy.get('[ng-click="$ctrl.deleteArticle()"]:first').click();
+  })
+
+})
