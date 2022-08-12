ce5de56bbc495a5a074c8d4532457cea5db338e9
Aaron Carrasco <vaaroncarrasco@gmail.com>
2022-08-12 06:33:15 -0500

    home tests improved

--- cypress/e2e/shared/home_loggedIn.cy.js
+++ cypress/e2e/shared/home_loggedIn.cy.js
@@ -1,37 +1,10 @@
 describe('Conduit homepage testing', () => {
 
-  it('Homepage without user logged in renders correctly', () => {
-    cy.visit('/')
-
-    // Banner is visible
-    cy.get('.banner')
-      .should('be.visible');
-
-    // Navbar links for logged out user are visible
-    cy.get('[show-authed="false"] > :nth-child(2) > .nav-link')
-      .should('have.attr', 'href', '#/login');
-
-    cy.get('[show-authed="false"] > :nth-child(3) > .nav-link')
-      .should('have.attr', 'href', '#/register');
-
-    // Your feed link is visible
-    cy.get('.feed-toggle > .nav')
-      .should('contain', 'Global Feed');
-
-    // Articles rendered
-    cy.get(':nth-child(1) > .article-preview')
-      .should('be.visible');
-
-    // Popular tag exists
-    cy.contains('Popular Tags')
-      .should('be.visible');
-
-    // Github footer link is visible
-    cy.footerDisplays();
+  beforeEach(() => {
+    cy.login()
   })
 
   it('Homepage with user logged in renders correctly', () => {
-    cy.login()
 
     // Banner is not visible anymore
     cy.get('.banner').should('not.be.visible');
@@ -44,7 +17,7 @@ describe('Conduit homepage testing', () => {
       .should('have.attr', 'href', '#/settings');
 
     cy.get('[show-authed="true"] > :nth-child(4) > .nav-link')
-      .should('have.attr', 'href', `#/@${username}`);
+      .should('have.attr', 'href', `#/@${localStorage.getItem('username')}`);
 
     // Your feed link is visible
     cy.get('.feed-toggle > .nav > li:first')
